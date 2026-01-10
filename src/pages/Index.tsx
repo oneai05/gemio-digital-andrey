cf500c27743717b7b8846a14c5ffef828e6ea190
asafecesar95 <asafe.cesar@unifesp.br>
2026-01-10 14:45:49 -0300

    Remove página Home e botão Voltar: simplifica fluxo para acesso direto à introdução do check-in sem navegação adicional

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import TypeformCheckin from "@/components/typeform/TypeformCheckin";
-import Home from "@/components/Home";
 
 const Index: React.FC = () => {
-  const [showHome, setShowHome] = useState(false);
-
-  if (showHome) {
-    return <Home onStartCheckin={() => setShowHome(false)} />;
-  }
-
-  return <TypeformCheckin onBack={() => setShowHome(true)} />;
+  return <TypeformCheckin />;
 };
 
 export default Index;
