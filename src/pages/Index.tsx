1b2dfe7e609c2f3c70202b28710a876ac0216c13
asafecesar95 <asafe.cesar@unifesp.br>
2026-01-10 14:50:48 -0300

    Restaura página Home e funcionalidade do botão Voltar: página inicial agora é Home e botão Voltar no formulário retorna para ela

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import TypeformCheckin from "@/components/typeform/TypeformCheckin";
+import Home from "@/components/Home";
 
 const Index: React.FC = () => {
-  return <TypeformCheckin />;
+  const [showCheckin, setShowCheckin] = useState(false);
+
+  if (showCheckin) {
+    return <TypeformCheckin onBack={() => setShowCheckin(false)} />;
+  }
+
+  return <Home onStartCheckin={() => setShowCheckin(true)} />;
 };
 
 export default Index;
